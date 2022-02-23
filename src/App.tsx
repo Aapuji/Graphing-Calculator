e7acc3548944103dac5e3b35dcbf2701add71348
Aapuji <om.robotics16@gmail.com>
2022-02-22 21:44:22 -0500

    Added Dragable Feature Also added resizable component, though it is probably not needed

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,10 @@ import logo from './logo.svg';
 import Graph  from './graph/Graph';
 import EqSidebar from './sidebar/EqSidebar';
 import './App.css';
-
-// function Resizeable({ children }: any) {
-//   let width = (props: any) => props.width;
-
-//   const [size, setSize] = useState({ x: 400, y: 300 });
-//   const ref = useRef() as MutableRefObject<HTMLDivElement>;
-
-//   const handler = useCallback(() => {
-//     function onMouseMove(e: MouseEvent) {
-//       setSize(currentSize => ({ 
-//         x: currentSize.x + e.movementX, 
-//         y: currentSize.y + e.movementY 
-//       }));
-//     }
-//     function onMouseUp() {
-//       ref.current.removeEventListener("mousemove", onMouseMove);
-//       ref.current.removeEventListener("mouseup", onMouseUp);
-//     }
-//     ref.current.addEventListener("mousemove", onMouseMove);
-//     ref.current.addEventListener("mouseup", onMouseUp);
-//   }, []);
-
-//   return (
-//     <div width={size.x} height={size.y}>
-//       <button ref={ref} onMouseDown={handler} pos={1} />
-//       {children}
-//     </div>
-//   );
-// }
-
-
-
+import './sidebar/Resizable.tsx';
+import { Resizeable, Axis } from './sidebar/Resizable';
+import Draggable from 'react-draggable';
+import { getPositionOfLineAndCharacter } from 'typescript';
 
 function App(props: any){
   let sidebarWT = window.innerWidth / 3, sidebarHT;
@@ -43,32 +15,36 @@ function App(props: any){
   const ref = useRef() as MutableRefObject<HTMLDivElement>;
   
   // Sidebar
-  useEffect(() => {
-    console.log(
-      'width', `: ${ref.current.getBoundingClientRect().x}`,
-      'height',  `: ${ref.current.getBoundingClientRect().y}`
-      );
-      sidebarWT = 1
-  }, []);
+  // useEffect(() => {
+  //   console.log(
+  //     'width', `: ${ref.current.getBoundingClientRect().x}`,
+  //     'height',  `: ${ref.current.getBoundingClientRect().y}`
+  //     );
+  //     // sidebarWT = 1
+  // }, []);
 
   // Mouse Drag Handler
-  const handler = useCallback(() => {
-    function onMouseMove(e: MouseEvent) {
-      setSize((currentSize: any) => ({
-        width: currentSize.width + e.movementX,
-        height: currentSize.height
-      }));
-    }
+  // const handler = useCallback(() => {
+  //   function onMouseMove(e: MouseEvent) {
+  //     setSize((currentSize: any) => ({
+  //       width: currentSize.width + e.movementX,
+  //       height: currentSize.height
+  //     }));
+  //   }
+  //   console.log(size.width, '\n', size.height);
+  //   function onMouseUp() {
+  //     ref.current.removeEventListener('mousemove', onMouseMove);
+  //     ref.current.removeEventListener('mouseup', onMouseUp);
+  //   }
 
-    function onMouseUp() {
-      ref.current.removeEventListener('mousemove', onMouseMove);
-      ref.current.removeEventListener('mouseup', onMouseUp);
-    }
+  //   ref.current.addEventListener('mousemove', onMouseMove);
+  //   ref.current.addEventListener('mouseup', onMouseUp);
+  // }, []);
 
-    ref.current.addEventListener('mousemove', onMouseMove);
-    ref.current.addEventListener('mouseup', onMouseUp);
-  }, []);
-
+  const [pos, setPos] = useState({ x: 1, y: 1 });
+  const trackPos = (data: {x: number, y: number}) => {
+    setPos({ x: data.x, y: data.y });
+  }
 
   return <>
     <div className="App">
@@ -89,11 +65,14 @@ function App(props: any){
 
 
       <main className="flex-container">
-        <div id="sidebar-eq" className="flex-container">
+        <div id="sidebar-eq" className="flex-container" draggable="true">
           <EqSidebar style={{width: sidebarWT - 5}}/>
-          <div id="eq-graph-border" ref={ref} onMouseDown={handler} draggable="true"></div>
+          <Draggable onDrag={(e, data) => trackPos(data)}  axis='x'>
+            <div id="eq-graph-border" draggable="true"></div>
+          </Draggable>
         </div>
         <Graph style={{width: (window.innerWidth - sidebarWT) + 'vw'}}/>
+
       </main>
     </div>
   </>;
--- src/sidebar/EqSidebar.tsx
+++ src/sidebar/EqSidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Draggable from "react-draggable";
 
 function EqSidebar(props: any) {
     return <div id="eqbar" style={props.style}>
