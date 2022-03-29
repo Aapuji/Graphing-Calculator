001d25bd21848acf2e47335cdc65c9bb03589a7e
Aapuji <om.robotics16@gmail.com>
2022-03-28 21:21:25 -0400

    Got the cells ot be text-editable, but need to fix overflow and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -5,85 +5,6 @@ import './App.css';
 import Cell from './cells/Cell';
 import FadeCell from './cells/FadeCell';
 
-// function App(props: any){
-//   let sidebarWT = window.innerWidth / 3, sidebarHT;
-
-//   const [size, setSize] = useState({ width: sidebarWT, height: sidebarHT });
-//   const ref = useRef() as MutableRefObject<HTMLDivElement>;
-  
-//   // Sidebar
-//   // useEffect(() => {
-//   //   console.log(
-//   //     'width', `: ${ref.current.getBoundingClientRect().x}`,
-//   //     'height',  `: ${ref.current.getBoundingClientRect().y}`
-//   //     );
-//   //     // sidebarWT = 1
-//   // }, []);
-
-//   // Mouse Drag Handler
-//   // const handler = useCallback(() => {
-//   //   function onMouseMove(e: MouseEvent) {
-//   //     setSize((currentSize: any) => ({
-//   //       width: currentSize.width + e.movementX,
-//   //       height: currentSize.height
-//   //     }));
-//   //   }
-//   //   console.log(size.width, '\n', size.height);
-//   //   function onMouseUp() {
-//   //     ref.current.removeEventListener('mousemove', onMouseMove);
-//   //     ref.current.removeEventListener('mouseup', onMouseUp);
-//   //   }
-
-//   //   ref.current.addEventListener('mousemove', onMouseMove);
-//   //   ref.current.addEventListener('mouseup', onMouseUp);
-//   // }, []);
-
-//   const [pos, setPos] = useState({ x: 1, y: 1 });
-//   const trackPos = (data: {x: number, y: number}) => {
-//     setPos({ x: data.x, y: data.y });
-//   }
-
-//   return <>
-//     {/* <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header> */}
-
-
-//       <main className="flex-container">
-//         {/* <div id="sidebar-eq" className="flex-container" draggable="true">
-//           <EqSidebar style={{width: sidebarWT - 5}}/>
-//           <Draggable onDrag={(e, data) => trackPos(data)}  axis='x'>
-//             <div id="eq-graph-border" draggable="true"></div>
-//           </Draggable>
-//         </div>
-//         <Graph style={{width: (window.innerWidth - sidebarWT) + 'vw'}}/> */}
-//         <div className="resizable" ref={ref} style={{
-//           resize: 'horizontal', 
-//           backgroundColor: 'blue',
-//           border: '2px solid',
-//           padding: '20px',
-//           width: '300px',
-//           overflow: 'auto'}}>
-//             Hello
-//             <div style={{width: '5px'}}></div>
-//           </div>
-//       </main>
-//     {/* </div> */}
-//   </>;
-// }
-
 function App() {
   const sidebarRef = useRef() as MutableRefObject<HTMLDivElement>;
   const [isResizing, setIsResizing] = useState(false);
@@ -124,10 +45,11 @@ function App() {
         ref={sidebarRef}
         className="app-sidebar"
         style={{ width: sidebarWidth }}
-        onMouseDown={(e) => e.preventDefault()}
+        // onMouseDown={(e) => e.preventDefault()}
       >
         <div className="app-sidebar-content" style={{backgroundColor: 'white'}}>
           <div id="expression-list-header" style={{height: '50px', backgroundColor: 'green'}}/>
+          <Cell />
           <Cell />
           <FadeCell />
         </div>
@@ -137,6 +59,7 @@ function App() {
         <Graph />
       </div>
     </div>
+    
   );
 }
 
--- src/cells/Cell.tsx
+++ src/cells/Cell.tsx
@@ -3,12 +3,20 @@ import './Cell.css';
 
 function Cell(props: any): JSX.Element {
 
-
+    let input: string;
+    let newChar: string;
 
     return <div className="eqbar-cell">
         <div className="cell-drag-icon"></div>
-        <div className="cell-content flex-container">
-            <div className="cell-text expression">
+        <div className="cell-content flex-container">   
+            <div
+                className="cell-text expression"
+                contentEditable='true'
+                onInput={e => {
+                    input = e.currentTarget.textContent!;
+                    newChar = input.slice(-1);
+                    console.log('Text inside div: ', input)}}
+            >
                 Hello
             </div>
         </div>
